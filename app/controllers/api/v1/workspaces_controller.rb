ab7bde4e530453986cf1bf036ccfd1974bf5e7f5
Chang Li <lichang@oregonstate.edu>
2018-11-29 16:18:10 +0800

    api for individual restaurant

--- app/controllers/api/v1/workspaces_controller.rb
+++ app/controllers/api/v1/workspaces_controller.rb
@@ -4,5 +4,15 @@ class Api::V1::WorkspacesController < Api::V1::BaseController
     puts @workspaces
     render json: @workspaces
   end
-end
 
+  def show
+    @workspaces = Workspace.find(params[:id])
+    render json: @workspaces
+  end
+
+  private
+  def set_workspace
+    # @workspaces = Workspace.find(params[:id])
+    # render json: @workspaces
+  end
+end
